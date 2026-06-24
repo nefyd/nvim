aa4d0fa58bd6ce81bbe0c0fcd3d6ef44700a9fc9
nefyd <nefydhp@gmail.com>
2026-06-24 12:25:10 +0800

    cuztomize status line

--- lua/nefyd/plugins/lualine.lua
+++ lua/nefyd/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       theme = "auto",
-      component_separators = { left = "│", right = "│" },
+      component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       disabled_filetypes = {
         statusline = { "oil" },
@@ -13,15 +13,13 @@ return {
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_b = { "diagnostics" },
       lualine_c = {
-        {
-          "filename",
-          path = 0
-        }
+        { "%=" },
+        { "filename", path = 0 },
       },
-      lualine_x = { "filetype" },
-      lualine_y = { "progress" },
+      lualine_x = { "branch" },
+      lualine_y = {},
       lualine_z = { "location" },
     },
   },
