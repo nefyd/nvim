99d0df91f831bda6b707fe0a6e23071134790e55
nefyd <nefydhp@gmail.com>
2026-08-01 22:48:22 +0800

    fix clipboard freeze

--- lua/nefyd/options.lua
+++ lua/nefyd/options.lua
@@ -28,15 +28,3 @@ vim.opt.termguicolors = true
 --- clipboard ---
 
 vim.opt.clipboard = "unnamedplus"
-
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
-}
